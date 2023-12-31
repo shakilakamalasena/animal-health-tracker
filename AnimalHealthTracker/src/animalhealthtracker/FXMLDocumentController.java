01d329fda5d1cc68759972c58941bb3c28d13101
Shakila Kamalasena <104765058+shakilakamalasena@users.noreply.github.com>
2023-12-31 08:16:33 +0530

    Designed the login and register forms and made the switching of those pages.

---
+++ AnimalHealthTracker/src/animalhealthtracker/FXMLDocumentController.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ */
+package animalhealthtracker;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ *
+ * @author Shakila Kamalasena
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private CheckBox login_checkBtn;
+
+    @FXML
+    private AnchorPane login_form;
+
+    @FXML
+    private Button login_loginBtn;
+
+    @FXML
+    private PasswordField login_password;
+
+    @FXML
+    private Hyperlink login_registerHere;
+
+    @FXML
+    private TextField login_showPassword;
+
+    @FXML
+    private ComboBox<?> login_user;
+
+    @FXML
+    private TextField login_username;
+
+    @FXML
+    private AnchorPane main_form;
+
+    @FXML
+    private CheckBox register_checkBox;
+
+    @FXML
+    private TextField register_email;
+
+    @FXML
+    private AnchorPane register_form;
+
+    @FXML
+    private Hyperlink register_loginHere;
+
+    @FXML
+    private PasswordField register_password;
+
+    @FXML
+    private TextField register_showPassword;
+
+    @FXML
+    private Button register_signupBtn;
+
+    @FXML
+    private TextField register_username;
+    
+    
+    //Switching login form and register form
+    public void switchForm(ActionEvent event){
+        
+        if(event.getSource() == login_registerHere){
+            login_form.setVisible(false);
+            register_form.setVisible(true);
+        }else if(event.getSource() == register_loginHere){
+            login_form.setVisible(true);
+            register_form.setVisible(false);
+        }
+        
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
